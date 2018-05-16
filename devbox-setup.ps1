3c75f487f48f41e3d1408f904d1dc475bb2fe680
Johan Bergström <johan.bergstrom@academicwork.se>
2018-05-16 11:09:29 +0200

    removed vs2017 for now

--- devbox-setup.ps1
+++ devbox-setup.ps1
@@ -6,8 +6,8 @@ Set-ExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFi
 
 
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1 # Show all folders in side panel
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1 #launch explorer to This PC
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
 
 
@@ -18,14 +18,19 @@ choco install -y sublimetext3.PackageControl
 choco install -y vscode
 choco install -y slack
 choco install -y firefox
+choco install -y insomnia-rest-api-client
+choco install -y fiddler4
+choco install -y 7zip
+choco install -y microsoft-teams
+choco install -y git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"'
 
 #--- Visual Studio 2017 ---
-choco install -y visualstudio2017professional
+#choco install -y visualstudio2017professional
 
 #--- VS Workloads ---
-choco install -y visualstudio2017-workload-netweb
-choco install -y visualstudio2017-workload-azure
-choco install -y visualstudio2017-workload-netcoretools
+#choco install -y visualstudio2017-workload-netweb
+#choco install -y visualstudio2017-workload-azure
+#choco install -y visualstudio2017-workload-netcoretools
 
 
 choco install -y Microsoft-Hyper-V-All -source windowsFeatures
@@ -33,13 +38,12 @@ choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
 
 choco install -y gitextensions
 choco install -y kdiff3
-choco install -y 7zip
 choco install -y curl
-choco install -y microsoft-teams
 
 choco install -y nodejs.install
 choco install -y poshgit
 choco install -y powershell-core
+choco install -y azurepowershell
 choco install -y resharper
 choco install -y yarn
 choco install -y python 
